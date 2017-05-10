f20a4bac2e72439cc4462cc36525049438ccc5ea
cuirj <cuirj@mingyuanyun.com>
2017-05-11 01:19:36 +0800

    1.查看全文全部内容显示 2.如果该条记录包含图片，则该条记录不入库

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # qsbk_spider
 抓取糗事百科纯文本段子
+
+抓取的地址为：
+http://www.qiushibaike.com/ 以及 http://www.qiushibaike.com/text/ 和 http://www.qiushibaike.com/textnew/ 这三个页面下的纯文本段子。
+
+抓取语言: 
+    
+- python3 + scrpay + mysql 存储，数据库表名为qb_content
+
+爬虫运行:
+    
+- cmd中 切换到相应目录，scrapy crawl qsbk
+
+说明:
+
+-     1.爬虫中已经使用了随机浏览器头(将浏览器头信息保存到了一个文件中)
+-     2.使用ip代理，用了阿布云，使用高匿ip抓取成功，不被墙的几率要高好多
+-     3.如果爬取结果状态码是[500, 503, 504, 400, 403, 404, 408]，则重试(retry下载中间件)
+-     4.由于目前这几个地址总页码都是35，所以，代码里直接写死了，建议是解析出来页码信息，最好不要写死
+
+ps：此爬虫是自己没事做着玩的，后面会继续完善
--- localtest/spiders/qsbk.py
+++ localtest/spiders/qsbk.py
@@ -6,8 +6,8 @@ from localtest.items import QiushibaikeItem
 
 class QsbkSpider(scrapy.Spider):
     name = "qsbk"
-    allowed_domains = ["www.qiushibaike.com"]
-    start_urls = ['http://www.qiushibaike.com/']
+    allowed_domains = ["qiushibaike.com"]
+    # start_urls = ['http://www.qiushibaike.com/','http://www.qiushibaike.com/textnew/','http://www.qiushibaike.com/text/']
     par_url = "http://www.qiushibaike.com"
 
     custom_settings = {
@@ -24,13 +24,24 @@ class QsbkSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        for i in range(1,3):
-            url = "http://www.qiushibaike.com/8hr/page/"+str(i)
-            yield Request(url,callback=self.parse)
+        for i in range(1,36):
+            url_index = "http://www.qiushibaike.com/8hr/page/"+str(i)
+            url_text = "http://www.qiushibaike.com/text/page/"+str(i)
+            url_textnew = "http://www.qiushibaike.com/textnew/page/"+str(i)
+            yield Request(url_index,callback=self.parse)
+            yield Request(url_text,callback=self.parse)
+            yield Request(url_textnew,callback=self.parse)
 
     def parse(self, response):
         for row in response.xpath('//a[@class="contentHerf"]'):
             itemss = QiushibaikeItem()
+            #如果有图片的情况这条就直接舍弃
+            #取图片xpath
+            thumb_class = row.xpath('following-sibling::div[1]/@class').extract_first()
+            # print(thumb_class)
+            if thumb_class == 'thumb':
+                continue
+
             url = row.xpath('@href').extract_first()
             itemss['url'] = self.par_url + url
             result = row.xpath('div[@class="content"]/span').extract()
